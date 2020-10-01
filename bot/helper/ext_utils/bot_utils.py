c46f828adda5c78f8fc566500e1efa4a0e282e65
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-10-01 14:01:48 +0100

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -92,20 +92,20 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>Filename :</b> <code>{download.name()}</code>"
+            msg += f"<b>📁 Filename :</b> <b>{download.name()}</b>"
             msg += f"\n<b>Status :</b> <i>{download.status()}</i>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
-                       f"\n<b>Downloaded :</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}" \
-                       f"\n<b>Speed :</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
+                msg += f"\n<b>{get_progress_bar_string(download)} {download.progress()}</b>" \
+                       f"\n<b>💾 Downloaded :</b> {get_readable_file_size(download.processed_bytes())} Of {download.size()}" \
+                       f"\n<b>🚀 Speed :</b> {download.speed()}, \n<b>⏱ ETA:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
-                    msg += f"\n<b>Info :- Seeders:</b> {download.aria_download().num_seeders}" \
-                        f" & <b>Peers :</b> {download.aria_download().connections}"
+                    msg += f"\n<b>ℹ Info :- Seeders:</b> {download.aria_download().num_seeders}" \
+                        f" & <b>ℹ Peers :</b> {download.aria_download().connections}"
                 except:
                     pass
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<b>GID</b>: <code>{download.gid()}</code>"
+                msg += f"\n<b>❌ GID</b>: <code>/cancel {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
